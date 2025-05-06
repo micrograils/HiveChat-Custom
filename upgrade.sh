7b292f467e7c69b1a39ec08e54edfbceaebbee56
MicroGrails <aaron@micrograils.com>
2025-05-06 15:16:24 +0800

    Update HiveChat submodule to track main branch

--- upgrade.sh
+++ upgrade.sh
@@ -1,13 +1,6 @@
-# HiveChat-Custom
+#!/bin/bash
 
-```bash
-git clone https://github.com/micrograils/HiveChat-Custom.git
-cd HiveChat-Custom
 git submodule update --init --recursive
-```
-
-```bash
 git add .
 git commit -m "Update HiveChat submodule to track main branch"
 git push
-```
